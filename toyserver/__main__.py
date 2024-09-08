d8076aad53bf7c44771175288183ee96a43c694f
Vadim Suharnikov <vsuahrnikov@gmail.com>
2024-09-08 09:33:03 +0000

    Refactor docker-compose.yml and uvicorn.run() function to work from local net

--- toyserver/__main__.py
+++ toyserver/__main__.py
@@ -1,6 +1,18 @@
+from argparse import ArgumentParser
+
 import uvicorn
 
 from toyserver.server import app
 
+
+def main() -> None:
+    """Run the server."""
+    parser = ArgumentParser()
+    parser.add_argument('--host', type=str, default='0.0.0.0')  # noqa: S104
+    parser.add_argument('--port', type=int, default=8081)
+    args = parser.parse_args()
+    uvicorn.run(app, host=args.host, port=args.port)
+
+
 if __name__ == '__main__':
-    uvicorn.run(app, host='0.0.0.0', port=8081)  # noqa: S104
+    main()
