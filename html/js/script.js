f7a736bfe793ac88b481b72cc0b9f89b05a74946
Vadim Suharnikov <vsuharnikov@gmail.com>
2024-11-26 08:48:53 +0000

    Add event handling for new submit button with code option

--- html/js/script.js
+++ html/js/script.js
@@ -5,11 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
     init_lib();
 
     const submit_button = document.getElementById('submit-button');
-    submit_button.addEventListener('click', () => submitButtonHandler(submit_button));
+    submit_button.addEventListener('click', () => submitButtonHandler(submit_button, false));
 
 
     const submit_button_with_code = document.getElementById('submit-button-with-code');
-    submit_button_with_code.addEventListener('click', () => submitButtonHandler(submit_button_with_code));
+    submit_button_with_code.addEventListener('click', () => submitButtonHandler(submit_button_with_code, true));
 
     const rerun_button = document.getElementById('rerun-button');
     rerun_button.addEventListener('click', rerun_button_handler);
